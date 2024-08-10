ea2217d10a37de3719b1283b86a04f120afbb1fb
sujeet-93 <sujeet93@live.com>
2024-08-10 12:16:11 +0530

    RC-01: Code to create registration form

--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import {
   CButton,
   CCard,
@@ -10,11 +10,44 @@ import {
   CInputGroup,
   CInputGroupText,
   CRow,
+  CAlert,
 } from '@coreui/react'
 import CIcon from '@coreui/icons-react'
 import { cilLockLocked, cilUser } from '@coreui/icons'
 
 const Register = () => {
+  const [showAlert, setShowAlert] = useState(false)
+  const [alertMessage, setAlertMessage] = useState('')
+  const [alertType, setAlertType] = useState('')
+  const username = useRef()
+  const email = useRef()
+  const password = useRef()
+  const cnfPassword = useRef()
+  const addPosts = async () => {
+    if (username.current.value === '') {
+      displayAlertMessage('warning', 'Enter Username', true)
+    } else if (email.current.value === '') {
+      displayAlertMessage('warning', 'Enter Email', true)
+    } else if (password.current.value === '') {
+      displayAlertMessage('warning', 'Enter Password', true)
+    } else if (cnfPassword.current.value === '') {
+      displayAlertMessage('warning', 'Enter Confirm password', true)
+    } else {
+      displayAlertMessage('', '', false)
+      let bobyContent = new Object()
+      bobyContent.username = username.current.value
+      bobyContent.email = email.current.value
+      bobyContent.password = password.current.value
+      console.log(bobyContent)
+    }
+  }
+
+  function displayAlertMessage(alertType, message, isShowAlert) {
+    setShowAlert(isShowAlert)
+    setAlertMessage(message)
+    setAlertType(alertType)
+  }
+
   return (
     <div className="bg-body-tertiary min-vh-100 d-flex flex-row align-items-center">
       <CContainer>
@@ -25,15 +58,16 @@ const Register = () => {
                 <CForm>
                   <h1>Register</h1>
                   <p className="text-body-secondary">Create your account</p>
+
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
                       <CIcon icon={cilUser} />
                     </CInputGroupText>
-                    <CFormInput placeholder="Username" autoComplete="username" />
+                    <CFormInput placeholder="Username" autoComplete="username" ref={username} />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>@</CInputGroupText>
-                    <CFormInput placeholder="Email" autoComplete="email" />
+                    <CFormInput placeholder="Email" autoComplete="email" ref={email} />
                   </CInputGroup>
                   <CInputGroup className="mb-3">
                     <CInputGroupText>
@@ -43,6 +77,7 @@ const Register = () => {
                       type="password"
                       placeholder="Password"
                       autoComplete="new-password"
+                      ref={password}
                     />
                   </CInputGroup>
                   <CInputGroup className="mb-4">
@@ -53,12 +88,19 @@ const Register = () => {
                       type="password"
                       placeholder="Repeat password"
                       autoComplete="new-password"
+                      ref={cnfPassword}
                     />
                   </CInputGroup>
                   <div className="d-grid">
-                    <CButton color="success">Create Account</CButton>
+                    <CButton color="success" onClick={() => addPosts()}>
+                      Create Account
+                    </CButton>
                   </div>
                 </CForm>
+                {showAlert ? <br /> : ''}
+                <CAlert color={alertType} visible={showAlert}>
+                  {alertMessage}
+                </CAlert>
               </CCardBody>
             </CCard>
           </CCol>
